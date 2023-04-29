f6f413bf1c99b98fa9d22e433f3319d94494dfe9
Erkan <erkankolakan@gmail.com>
2023-04-29 14:38:13 +0300

    js ile temel attık

---
+++ app.js
@@ -0,0 +1,4 @@
+const player = new MusicPlayer(musicList); //MusicPlayer classı içerisinden musicList bilgisini al.
+
+let  music = player.getMusic(); //player sayesinde şarkıcı bilgilerini alır ve 
+
--- musicplayer.js
+++ musicplayer.js
@@ -1,7 +1,28 @@
 class MusicPlayer {
     constructor(musicList){
-        this.musicList = this.musicList 
+        this.musicList = musicList 
         this.index = 0;
         // MusicPlayerden bir nesne oluşturduğum anda index numarası 0 a eşitlensin isterim. Ancak ben bir sonraki şarkı dediğim zaman index numarası ile oynarak örneğin 1 ekleyerek bir sonraki index numarasına gidip ordaki bilgileri çekmiş olacakğım.
+    }
+
+    getMusic() {
+        return this.musicList[this.index]
+    }
+
+    next(){
+        if (this.musicList.length != this.index + 1 ) {
+            this.index++;
+        }
+        else {
+            this.index=0;
+        }
+    }
+    previous(){
+        if (this.index != 0  ) {
+            this.index--;
+        }
+        else {
+            this.index = this.musicList.length - 1
+        }
     }
 }

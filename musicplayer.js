ad7633e723da03d33081fc0d78082c8d6f69fd6e
Erkan <erkankolakan@gmail.com>
2023-04-29 14:37:00 +0300

    ana iskeleti oluştruk

---
+++ musicplayer.js
@@ -0,0 +1,7 @@
+class MusicPlayer {
+    constructor(musicList){
+        this.musicList = this.musicList 
+        this.index = 0;
+        // MusicPlayerden bir nesne oluşturduğum anda index numarası 0 a eşitlensin isterim. Ancak ben bir sonraki şarkı dediğim zaman index numarası ile oynarak örneğin 1 ekleyerek bir sonraki index numarasına gidip ordaki bilgileri çekmiş olacakğım.
+    }
+}
